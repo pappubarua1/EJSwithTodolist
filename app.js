15fbe35e5cc0978eacfe1efc89f840227fe6d0dc
Pappu Barua <pappubarua95@gmail.com>
2021-03-15 01:30:43 +0600

    ejs templeting

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const path = require('path');
 const app = express();
 
 let items = ["Buy Food","Cook Food","Eat Food"];
+let workItem = [];
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -24,15 +25,37 @@ app.get("/", function(req,res){
     let day = today.toLocaleDateString("en-US",options);
 
 
-res.render("list",{kindOfDay: day, newListItems:items});
+res.render("list",{listTitle: day, newListItems:items});
 
 }); 
-app.post("/",function(req,res){
-let item = req.body.newItem; 
-items.push(item);
-res.redirect("/");
 
+app.get('/work',function(req,res){
+res.render("list",{listTitle:"Work list", newListItems:workItem});
 });
+
+app.post("/work",function(req,res){
+    let item = req.body.newItem;
+    workItem.push(item);
+    res.redirect("/work");
+});
+
+app.post("/",function(req,res){
+
+let item = req.body.newItem; 
+
+if(req.body.list === "Work"){
+workItem.push(item);
+res.redirect("/work");
+}
+else{
+    items.push(item);
+    res.redirect("/");
+}
+});
+app.get("/about",function(req,res){
+    res.render("about");
+})
+
 app.listen(3000,function(){
 console.log("server port is running 3000");
 });
